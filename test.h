5e4566245bd939079483c507a209ec17f2ecd549
Brian Rasmussen <brian.rasmussen55@gmail.com>
2018-07-16 17:13:41 -0600

    moved test to own files

---
+++ test.h
@@ -0,0 +1,16 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+
+
+void runTest();
+void test1();
+void test2();
+void test3();
+void test4();
+void printFailed(std::string msg);
+void printSuccess(std::string msg);
+
+
+#endif
